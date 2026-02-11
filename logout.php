c9b277f364341e0fa3fbd9aafecc0aa7998889b1
Sagar Bhattarai <bhattaraisagar2001@gmail.com>
2026-02-11 08:31:16 +0545

    adding logout

---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php
+// logout.php - Log out the user and redirect to home page
+session_start();
+
+// Unset all session variables
+$_SESSION = array();
+
+// Destroy the session
+session_destroy();
+
+// Redirect to home page
+header('Location: front.php');
+exit();
+?>
